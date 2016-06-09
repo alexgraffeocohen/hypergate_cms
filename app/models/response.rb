79f3d301cd36746bb4a59cb016243121498b8269
Alex Wilkinson <alex.wilkinson@me.com>
2016-06-09 15:31:27 -0400

    Create Role

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,4 @@
 class Response < ActiveRecord::Base
   belongs_to :encounter
+  belongs_to :role
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609192703) do
+ActiveRecord::Schema.define(version: 20160609192943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20160609192703) do
     t.integer  "encounter_id",   null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
