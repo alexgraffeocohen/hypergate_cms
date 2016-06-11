5ddbfdae1d70b4bd0bdcb45555ba56a784ca8e60
Alex Wilkinson <alex.wilkinson@me.com>
2016-06-11 17:33:41 -0400

    Create some basic fixtures and load seeds in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  Rails.application.load_seed
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
