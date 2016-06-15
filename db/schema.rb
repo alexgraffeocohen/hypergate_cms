e7c26da56184a8fa9c99df54724be031d0d61e1b
Alex Wilkinson <alex.wilkinson@me.com>
2016-06-15 10:49:34 -0400

    Create Outcome, which connects any rewards, harms, upgrades and has a description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615143445) do
+ActiveRecord::Schema.define(version: 20160615144609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(version: 20160615143445) do
   end
 
   add_index "options", ["result_type", "result_id"], name: "index_options_on_result_type_and_result_id", using: :btree
+
+  create_table "outcomes", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "response_skill_checks", force: :cascade do |t|
     t.text     "success_text"
