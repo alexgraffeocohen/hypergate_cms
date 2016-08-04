6b7d9fda55619e267dacb56d8a910414941b04f7
Alex Wilkinson <alex.wilkinson@me.com>
2016-08-04 12:44:50 -0400

    kill dead code in NewEncounterService

--- app/services/new_encounter_service.rb
+++ app/services/new_encounter_service.rb
@@ -22,26 +22,4 @@ class NewEncounterService
   private
 
   attr_reader :attributes
-
-  def responses
-    attributes[:responses].select { |role_name, text|
-      text.present?
-    }.map do |role_name, text|
-      Response.new(
-        role: Role.find_by(name: role_name.titleize),
-        text: text
-      )
-    end
-  end
-
-  def options
-    attributes[:options].select { |order, text|
-      text.present?
-    }.map do |order, text|
-      Option.new(
-        order: order.to_i,
-        text: text
-      )
-    end
-  end
 end
