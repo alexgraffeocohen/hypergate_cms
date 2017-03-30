7466f9fcf80c29271ddf28ba6c535ff7b514921a
Alex Wilkinson <alex.wilkinson@me.com>
2017-03-29 22:58:03 -0400

    Add EncounterPresenter for Encounter show page

---
+++ app/presenters/encounter_presenter.rb
@@ -0,0 +1,25 @@
+class EncounterPresenter
+  attr_reader :encounter
+
+  def initialize(encounter)
+    @encounter = encounter
+  end
+
+  def title
+    encounter.title || default_title
+  end
+
+  def category
+    encounter.category.name.titleize
+  end
+
+  def description
+    encounter.description
+  end
+
+  private
+
+  def default_title
+    "Encounter ##{encounter.id}"
+  end
+end
---
+++ test/presenters/encounter_presenter_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class EncounterPresenterTest < ActiveSupport::TestCase
+  def setup
+    @encounter = encounters(:ai_planet)
+    @presenter = EncounterPresenter.new(@encounter)
+  end
+
+  test "#title displays encounter title when it's not nil" do
+    @encounter.title = "AI Planet"
+
+    assert_equal(@presenter.title, "AI Planet")
+  end
+
+  test "#title has a default when encounter title is nil" do
+    @encounter.title = nil
+    @encounter.category = categories(:habitable_planet)
+
+    assert_equal(
+      @presenter.title,
+      "Encounter ##{@encounter.id}"
+    )
+  end
+
+  test "#category displays encounter category in title case" do
+    @encounter.category = categories(:habitable_planet)
+
+    assert_equal(@presenter.category, "Habitable Planet")
+  end
+
+  test "#description displays encounter description" do
+    @encounter.description = "A Description"
+
+    assert_equal(@presenter.description, "A Description")
+  end
+end
