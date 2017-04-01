0d607e75ce2cdd28bd7f37e15a21e203150f9f52
Alex Wilkinson <alex.wilkinson@me.com>
2017-04-01 15:20:57 -0400

    Add Event edit functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,24 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    event = Event.find(params[:id])
+    @presenter = EventPresenter.new(event)
+  end
+
+  def update
+    event = Event.find(params[:id])
+
+    if event.update_attributes(event_params)
+      flash[:notice] = "Successfully updated event."
+      redirect_to encounter_path(encounter)
+    else
+      @event_presenter = EncounterPresenter.new(event)
+      flash[:error] = "There was a problem saving this event: #{event.errors.messages}"
+      render :edit
+    end
+  end
+
   private
 
   def event_params
