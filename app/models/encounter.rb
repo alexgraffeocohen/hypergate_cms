756c1bab051308334a0e746a1eab7bff79f9253f
Alex Wilkinson <alex.wilkinson@me.com>
2017-04-01 22:29:12 -0400

    Encounter: add dependent destroy to #events

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -2,7 +2,7 @@ class Encounter < ActiveRecord::Base
   belongs_to :category
   belongs_to :starting_event, class_name: Event
   has_one :trigger_event, class_name: Event, inverse_of: :next_encounter
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates_presence_of :category, :description, :standalone
   validates_presence_of :starting_event, if: :published
