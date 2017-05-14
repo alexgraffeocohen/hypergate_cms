aea79f5bc0bd8fe6c81d7299dd5874ef0391b12c
Alex Wilkinson <alex.wilkinson@me.com>
2017-05-13 20:19:00 -0400

    (m) delete inaccurate comment

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,6 @@ class Event < ActiveRecord::Base
   belongs_to :next_encounter, class_name: Encounter, inverse_of: :trigger_event
   has_many :options, dependent: :destroy
   has_many :responses, dependent: :destroy
-  # The records which get deleted are the join records, rather than the
-  # associated records.
-  # See: http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#method-i-has_many
   has_many :event_results, inverse_of: :event, dependent: :destroy
   belongs_to :item
   # The records which get deleted are the join records, rather than the
