46db137214e9d03e325e2b92cdc7f4fccca213a6
Alex Wilkinson <alex.wilkinson@me.com>
2017-05-15 23:10:19 -0400

    Add published attribute to encounter forms

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -58,6 +58,6 @@ class EncountersController < ApplicationController
   private
 
   def encounter_params
-    params.require(:encounter).permit(:category_id, :title, :description)
+    params.require(:encounter).permit(:category_id, :title, :description, :published)
   end
 end
